f5e4e9513ee27a1ed880f1f400fb34943562b2cb
suuk99 <jswook0422@naver.com>
2025-09-10 21:44:41 +0900

    게시판 프로그램_1

---
+++ src/com/project/java/Article.java
@@ -0,0 +1,15 @@
+package com.project.java;
+
+public class Article {
+	String title;
+	String content;
+	int id;
+	String now;
+	
+	Article(String title, String content, int id, String now) {
+		this.title = title;
+		this.content = content;
+		this.id = id;
+		this.now = now;
+	}
+}
